847ce2a9a3cb0272cc259967b61e0d16aa0dde09
Richard Morrison <richard@rmorrison.net>
2014-09-05 21:14:57 +0100

    Tidy up; remove view2

--- app/app.js
+++ app/app.js
@@ -4,9 +4,8 @@
 angular.module('myApp', [
   'ngRoute',
   'myApp.view1',
-  'myApp.view2',
   'myApp.version'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider.otherwise({redirectTo: '/'});
 }]);
--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -6,19 +6,19 @@ describe('my app', function() {
 
   browser.get('index.html');
 
-  it('should automatically redirect to /view1 when location hash/fragment is empty', function() {
-    expect(browser.getLocationAbsUrl()).toMatch("/view1");
+  it('should automatically redirect to / when location hash/fragment is empty', function() {
+    expect(browser.getLocationAbsUrl()).toMatch("/");
   });
 
 
   describe('view1', function() {
 
     beforeEach(function() {
-      browser.get('index.html#/view1');
+      browser.get('index.html#/');
     });
 
 
-    it('should render view1 when user navigates to /view1', function() {
+    it('should render view1 when user navigates to /', function() {
       expect(element.all(by.css('[ng-view] p')).first().getText()).
         toMatch(/partial for view 1/);
     });
@@ -26,17 +26,4 @@ describe('my app', function() {
   });
 
 
-  describe('view2', function() {
-
-    beforeEach(function() {
-      browser.get('index.html#/view2');
-    });
-
-
-    it('should render view2 when user navigates to /view2', function() {
-      expect(element.all(by.css('[ng-view] p')).first().getText()).
-        toMatch(/partial for view 2/);
-    });
-
-  });
 });
